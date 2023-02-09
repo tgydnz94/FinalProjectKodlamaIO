856a7ee61c678add7f99384722d6a11de872c174
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-02-10 02:59:08 +0300

    10.02.2023 Ders sonu

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -1,15 +1,14 @@
 ﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
-using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public interface IOrderDal : IEntityRepository<Order>
     {
-        List<ProductDetailDTO> GetProductDetails();
-
     }
 }
