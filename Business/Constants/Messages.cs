0f08d986768c85ffaa84191471c3e20b3aa737b6
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-07-08 03:36:33 +0300

    JWT

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
 		public static string ProductCountOfCategoryError="Kategori için izin verilen sayı aşıldı";
 		public static string ProductNameAlreadyExist="Bu isim kullanılıyor";
 		public static string CategoryLimitExceded="Limit aşıldı";
+		public static string AuthorizationDenied ="Yetkiniz yok";
+		public static string SuccessfulLogin="Giriş başarılı";
+		public static string UserNotFound="Kullanıcı bulunamadı";
+		public static string UserRegistered="Kayıtlı kullanıcı";
+		public static string UserAlreadyExists="Kullanıcı zaten var";
+		public static string PasswordError="Mail veya şifre hatalı";
+		public static string AccessTokenCreated="Token oluşturuldu";
 	}
 }
